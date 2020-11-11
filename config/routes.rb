cc843ad62b4a43fd3b4b17019549ae91ba8425f6
Elsa Lebas <lebas.elsa@gmail.com>
2020-11-11 16:12:01 +0100

    implement full user stories for resto and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # REFACTO
+  # --------------
+  # get "/restaurants/new", to: "restaurants#new" # Create a new Restaurant (CREATE>GET>#new)
+  # post "/restaurants", to: "restaurants#create" # Create a new Restaurant (CREATE>POST>#create)
+  # get "/restaurants", to: "restaurants#index" # All restaurants (READ>GET>#index)
+  # get "/restaurants/:id", to: "restaurants#show", as: :restaurant # Details of 1 restaurant (READ>GET>#show)
+
+  resources :restaurants, only: [:new, :create, :index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
